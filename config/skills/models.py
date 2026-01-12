671762a8b64dd3c1ee83d044a5be3ef16fed7807
Ian Cossa <ianrafaelelias@gmail.com>
2026-01-12 11:18:58 +0530

    setting the models across the apps

--- config/skills/models.py
+++ config/skills/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class StudentSkill(models.Model):
+    student = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE,related_name='skills')
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+    verified_by = models.ForeignKey(
+        User,
+        on_deletee=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='verified_skills'
+    )
+
+    verified_at: models.DateTimeField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.student} - {self.skill}"
+    
--- config/users/models.py
+++ config/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('STUDENT', 'student'),
+        ('COMPANY', 'company'),
+        ('UNIVERSITY', 'university'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    wallet_address = models.CharField(max_length=255, blank=True, null=True,
+        help_text="Blockchain wallet address (e.g., MetaMask)")
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
