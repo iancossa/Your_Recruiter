fe7dd1c62e051d1f17c6f57cc05421e61725e7ec
Ian Cossa <ianrafaelelias@gmail.com>
2026-01-14 15:16:39 +0530

    Templates

--- config/agreements/admin.py
+++ config/agreements/admin.py
@@ -4,5 +4,5 @@ from .models import Agreement
 # Register your models here.
 @ admin.register(Agreement)
 class AgreementAdmin(admin.ModelAdmin):
-    list_display =('student', 'company', 'internship', 'start_date', 'end_date', 'status')
-    list_filter = ('status', 'company', 'student')
+    list_display =('student',  'internship', 'status')
+    list_filter = ('status',)
--- config/users/admin.py
+++ config/users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
 class CustomerUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'role', 'wallet_address', 'is_active')
     list_filter = ('role',  'is_active')
